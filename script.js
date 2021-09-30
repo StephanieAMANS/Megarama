5558e4bfe08c92556bd85ddd87cb51b958162cfa
Stéphanie AMANS <stephanie-amans_student2021@wilder.school>
2021-09-30 15:04:24 +0200

    style css modification

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
-const myImg = document.querySelector("#actualFilms");
-
-myImg.addEventListener('mouseover', function(){
-    myImg.style.backgroundColor = "red";
-})
-myImg.addEventListener('mouseleave', function(){
-    myImg.style.backgroundColor = "white";
-})
-
 let message = "Essayez toujours ! Personne ne vous répondra 😜 ..."
 let button = document.getElementById("button");
 button.addEventListener("click", function (event) {
