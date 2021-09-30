26ba9b21a2b80deb6ed00faf280f96ef40fb4f1b
alikhyati <alikhyatihotmail.com>
2021-09-30 15:51:45 +0200

    commit navbar

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+function toggleMenu () {  
+  const navbar = document.querySelector('.navbar');
+  const burger = document.querySelector('.burger');
+  burger.addEventListener('click', (e) => {    
+    navbar.classList.toggle('show-nav');
+  });    
+};
+toggleMenu();
+
+
 let message = "Essayez toujours ! Personne ne vous répondra 😜 ..."
 let button = document.getElementById("button");
 button.addEventListener("click", function (event) {
